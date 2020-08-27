c2c976f5a68917b77217ae389c0ba92877208e6f
Monica <moniarbe233@gmail.com>
2020-08-27 12:56:24 -0500

    Updates

---
+++ precache-manifest.d16f5a4a76de0b39161bf782b118e6e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4ead92b7526600c169b182f173893d",
+    "url": "/IMC/index.html"
+  },
+  {
+    "revision": "2597b58406b46f3add56",
+    "url": "/IMC/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e6c3aba8de11282b1382",
+    "url": "/IMC/static/js/2.09d32d23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IMC/static/js/2.09d32d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2597b58406b46f3add56",
+    "url": "/IMC/static/js/main.4eefa950.chunk.js"
+  },
+  {
+    "revision": "2d472113104aada6fca6",
+    "url": "/IMC/static/js/runtime-main.97c192f6.js"
+  }
+]);
